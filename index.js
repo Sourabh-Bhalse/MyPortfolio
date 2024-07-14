8d9d49e4da77afcf0c7856942b08b80d17fc236e
Sourabh Bhalse <sourabhbhalse197@gmail.com>
2024-07-14 21:48:48 +0530

    portfolio

---
+++ index.js
@@ -0,0 +1,13 @@
+// var typed = new typed ("#text", {
+//     Strings: ["FrontEnd Developer", "Web Developer"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// });
+ 
+
+  var typed = new Typed('#element', {
+    strings: [' Front-end Developer', 'Web Developer','UI/UX Designer','Android App Developer'],
+    typeSpeed: 50,
+  });
